2f2d43cf6f7222e753b0754fc9a6e120c58b3bad
ConsKi123 <c1petremoto@gmail.com>
2025-10-22 12:38:41 -0300

    Update Cliente.cs

--- Proyecto C# UNAJ/Cliente.cs
+++ Proyecto C# UNAJ/Cliente.cs
@@ -9,20 +9,49 @@ namespace Proyecto_C__UNAJ
         //atributos
         private string nombre;
         private string apellido;
-        private int dni;
+        private string dni;
         private string direccion;
         private int telefono;
         private string email;
+       
         //properties
-        public string Nombre { get; set; }
+       /* public string Nombre { get; set; }
         public string Apellido { get; set; }    
         public int Dni { get; set; }  
         public string Direccion { get; set;}
         public int Telefono { get; set;}
-        public string Email { get; set;}    
+        public string Email { get; set;}  */
+
+        public string Nombre{
+		    get{return nombre;}
+		    set{nombre = value;}
+        }
+        public string Apellido{
+	        get{return apellido;}
+	        set{apellido = value;}
+        }
+	    public string Dni{
+	        get{dni nombre;}
+	        set{dni = value;}
+        }
+	    public string Direccion{
+		    get{return direcion;}
+		    set{direccion = value;}
+        }
+        public string Mail{
+	        get{return mail;}
+	        set{mail = value;}
+        }    
+        public int Numero{
+	        get{return numero;}
+	        set{numero = value;}
+        }
+
+
+        
         
         //constructores
-        public Cliente(string nom, string ape, int dni, string dire, int tel, string mail)
+        public Cliente(string nom, string ape, string dni, string dire, int tel, string mail)
         {
             this.Nombre = nom;
             this.Apellido = ape;    
